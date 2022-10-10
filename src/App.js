3407832a7cfa38e0c4ecbdb86d2a0bff7b5184c1
mcmak <maimcmak@outlook.com>
2022-10-10 17:22:17 +0800

    bloglist frontend 5.2

--- src/App.js
+++ src/App.js
@@ -15,18 +15,39 @@ const App = () => {
     )  
   }, [])
 
+  //get token
+  useEffect (() => {
+    const loggedUserJson = window.localStorage.getItem('loggedNoteappUser')
+    if(loggedUserJson) {
+      const user = JSON.parse(loggedUserJson)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       console.log(exception,'test')
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
   }
 
   const loginForm = ( ) => (
@@ -67,6 +88,9 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.username} logged in
+        <button onClick={handleLogout}>logout</button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
