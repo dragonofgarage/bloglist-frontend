8b2f35c4c74f12a8542659f89591f11666da5ec0
mcmak <maimcmak@outlook.com>
2022-10-18 20:00:48 +0800

    bloglist frontend 5.9

--- src/App.js
+++ src/App.js
@@ -145,7 +145,7 @@ const App = () => {
         />
       </Togglable>
         
-      {blogs.map(blog =>
+      {blogs.map(blog=>blog).sort((a,b) => a.likes > b.likes ? -1 : 1).map(blog =>
         <Blog key={blog.id} blog={blog} handleUpdateLikes={handleUpdateLikes}/>
       )}
     </div>
